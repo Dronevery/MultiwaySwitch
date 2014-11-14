d19c3a9aa772d2200d8e6714a26bb5abf35cb659
haiwei624 <haiwei624@163.com>
2014-11-14 22:35:35 +0800

    Fix bugs caused by using struct asock for UDP.

--- code/client.c
+++ code/client.c
@@ -19,11 +19,6 @@ struct netflag
     int arr[ARRLENGTH];
     int p,tot;
 };
-struct asock
-{
-    int sock;
-    struct sockaddr_in addr;
-};
 struct param
 {
     int id;
@@ -38,26 +33,25 @@ pthread_mutex_t sock_mutex[4] = {//for asock[4]
     PTHREAD_MUTEX_INITIALIZER,
     PTHREAD_MUTEX_INITIALIZER
 };
-    
+
 
 const int port_priority[3] = {0, 1, 2}; //means: port0 has the highest priority to be choosed, than port1,than port2 
 int netstatus[3];
 unsigned int tot;
 struct netflag Flags;
-struct asock asock[4];//sock0,1,2:net to server  sock3:localhost
-struct sockaddr_in ClientAddr;
+int Socket[4];//sock0,1,2:net to server  sock3:localhost
+struct sockaddr_in SendAddr[4];
+struct sockaddr_in RecvAddr[4];
 int PortNow;
 
 void init_mutex();
 int init_netflag(struct netflag *a);
 int update_netflag(struct netflag *a, int flag);
-int init_sock_c(struct asock *asock, char s_addr[], char s_port[]);
-int Sendto(struct asock *asock, char msg[]);
-int Recvfrom(struct asock *asock, char msg[]);
-int init_sock_s(struct asock *asock,  char s_port[]);
-int sendto_s(struct asock *asock, char msg[]);
-int recvfrom_s(struct asock *asock, char msg[]);
-int wait_recv(int Port, char msg[], int WaitTime);
+int init_sock_c(int PortId, char s_addr[], char s_port[]);
+int init_sock_s(int PortId,  char s_port[]);
+int Sendto(int PortId, char msg[]);
+int Recvfrom(int PortId, char msg[]);
+int wait_recv(int PortId, char msg[], int WaitTime);
 void * TestPort(void * v);
 void * SwitchPort(void * v);
 void * ForwardUDP(void * v);
@@ -84,9 +78,9 @@ int main(int argc, char **argv)
     for(pi = 0; pi < 3; pi++)
     {
         netstatus[pi] = 1;
-        init_sock_c(&asock[pi], argv[1], argv[2+pi]);
+        init_sock_c(pi, argv[1], argv[2+pi]);
     }
-    init_sock_s(&asock[3], argv[5]);
+    init_sock_s(3, argv[5]);
 
     pthread_t pt_t[3], pt_s, pt_f;
     pthread_create(&pt_s, NULL, SwitchPort, NULL);
@@ -123,7 +117,7 @@ void * TestPort(void * v)
         pthread_mutex_unlock(&tot_mutex);
 
         pthread_mutex_lock(&sock_mutex[id]);
-        Sendto(&asock[id], msg);//send msg
+        Sendto(id, msg);//send msg
         pthread_mutex_unlock(&sock_mutex[id]);
         //printf("sent: %s\n", msg);
 
@@ -199,13 +193,13 @@ void * ForwardUDP(void *v)
     while(1)
     {
         FD_ZERO(&inputs);
-        FD_SET(asock[3].sock, &inputs);
+        FD_SET(Socket[3], &inputs);
         int result = select(FD_SETSIZE, &inputs, NULL, NULL, NULL);  //wait until some messages arrive
         if(result <= 0)continue;
-        if(FD_ISSET(asock[3].sock, &inputs))
+        if(FD_ISSET(Socket[3], &inputs))
         {
             pthread_mutex_lock(&sock_mutex[3]);
-            Recvfrom(&asock[3], &buff[1]);
+            Recvfrom(3, &buff[1]);
             pthread_mutex_unlock(&sock_mutex[3]);
 
             printf("!>>Get UDP from local port<<!\n");
@@ -214,7 +208,7 @@ void * ForwardUDP(void *v)
             printf("%s\n", buff);
 
             pthread_mutex_lock(&sock_mutex[PortNow]);
-            Sendto(&asock[PortNow], buff);
+            Sendto(PortNow, buff);
             pthread_mutex_unlock(&sock_mutex[PortNow]);
         }
     }
@@ -261,45 +255,64 @@ int update_netflag(struct netflag *a, int flag)
     return 0;
 }
 
-int init_sock_c(struct asock *asock, char s_addr[], char s_port[])
+int init_sock_c(int PortId, char s_addr[], char s_port[])
 {
 
-    if((asock->sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    if((Socket[PortId] = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("init_socket");
         exit(1);
     }
-    asock->addr.sin_family = AF_INET;
-    asock->addr.sin_port = htons(atoi(s_port));
-    asock->addr.sin_addr.s_addr = inet_addr(s_addr);
-    if(asock->addr.sin_addr.s_addr == INADDR_NONE)
+    SendAddr[PortId].sin_family = AF_INET;
+    SendAddr[PortId].sin_port = htons(atoi(s_port));
+    SendAddr[PortId].sin_addr.s_addr = inet_addr(s_addr);
+    if(SendAddr[PortId].sin_addr.s_addr == INADDR_NONE)
     {
         printf("Incorrect ip address!\n");
-        close(asock->sock);
+        close(Socket[PortId]);
         exit(1);
     }
     return 0;
 }
-
-int Sendto(struct asock *asock, char msg[]) // sendto for client
+int init_sock_s(int PortId, char s_port[])
+{
+    
+    if((Socket[PortId] = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
+    {
+        perror("init_socket");
+        exit(1);
+    }
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(atoi(s_port));
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    
+    if(bind(Socket[PortId], (struct sockaddr *)&addr, sizeof(addr)) < 0)
+    {
+        perror("init_socket_bind");
+        exit(1);
+    }
+    return 0;
+}
+int Sendto(int PortId, char msg[]) // sendto for client
 {
     int n;
-    n = sendto(asock->sock, msg, strlen(msg), 0, (struct sockaddr *)&(asock->addr), sizeof(asock->addr));
+    n = sendto(Socket[PortId], msg, strlen(msg), 0, (struct sockaddr *)&(SendAddr[PortId]), sizeof(SendAddr[PortId]));
     if (n < 0)
     {
         perror("sendto");
-        //close(asock->sock);
+        //close(Socket[PortId]);
         /* print error but continue */
         return -1;
     }
     return 0;
 }
 
-int Recvfrom(struct asock *asock, char msg[]) //recvfrom for client
+int Recvfrom(int PortId, char msg[]) //recvfrom for client
 {
-    int addr_len = sizeof(asock->addr);
+    int addr_len = sizeof(RecvAddr[PortId]);
     int n;
-    n = recvfrom(asock->sock, msg, BUFFLENGTH, MSG_DONTWAIT, (struct sockaddr *)&(asock->addr), (socklen_t *)&addr_len);
+    n = recvfrom(Socket[PortId], msg, BUFFLENGTH, MSG_DONTWAIT, (struct sockaddr *)&(RecvAddr[PortId]), (socklen_t *)&addr_len);
     if (n>0)
         {
             msg[n] = 0;
@@ -311,13 +324,13 @@ int Recvfrom(struct asock *asock, char msg[]) //recvfrom for client
     return n;
 }
 
-int wait_recv(int Port, char msg[], int WaitTime)// Wait UDP response for WaitTime usec
+int wait_recv(int PortId, char msg[], int WaitTime)// Wait UDP response for WaitTime usec
 {
     static char buff[BUFFLENGTH];
     fd_set inputs;
     struct timeval timeout;
     FD_ZERO(&inputs);
-    FD_SET(asock[Port].sock, &inputs);
+    FD_SET(Socket[PortId], &inputs);
     timeout.tv_sec = WaitTime / 1000000;
     timeout.tv_usec = WaitTime % 1000000;
     while(timeout.tv_usec > 0 || timeout.tv_sec > 0)
@@ -326,13 +339,13 @@ int wait_recv(int Port, char msg[], int WaitTime)// Wait UDP response for WaitTi
         //printf("%d : %d : %d\n", result, (int)timeout.tv_sec, (int)timeout.tv_usec);
         if(result <= 0)
             return 0;
-        pthread_mutex_lock(&sock_mutex[Port]);
-        int n = Recvfrom(&asock[Port], buff);
-        pthread_mutex_unlock(&sock_mutex[Port]);
+        pthread_mutex_lock(&sock_mutex[PortId]);
+        int n = Recvfrom(PortId, buff);
+        pthread_mutex_unlock(&sock_mutex[PortId]);
         if(buff[0] == '3')
         {
             pthread_mutex_lock(&sock_mutex[3]);
-            Sendto(&asock[3], &buff[1]);
+            Sendto(3, &buff[1]);
             pthread_mutex_unlock(&sock_mutex[3]);
             continue;
         }
@@ -342,22 +355,3 @@ int wait_recv(int Port, char msg[], int WaitTime)// Wait UDP response for WaitTi
     return 0;
 }
 
-int init_sock_s(struct asock *asock, char s_port[])
-{
-    
-    if((asock->sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
-    {
-        perror("init_socket");
-        exit(1);
-    }
-    asock->addr.sin_family = AF_INET;
-    asock->addr.sin_port = htons(atoi(s_port));
-    asock->addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    
-    if(bind(asock->sock, (struct sockaddr *)&asock->addr, sizeof(asock->addr)) < 0)
-    {
-        perror("init_socket_bind");
-        exit(1);
-    }
-    return 0;
-}
